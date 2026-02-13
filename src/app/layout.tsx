a3b6445d42045dc7e82bf3f30e75490e4d3eaea4
trooper454 <tdavis25@gmail.com>
2026-02-13 15:57:27 -0700

    fixed google font import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils"; // from shadcn
 import { ServerSidebar } from "@/components/server-sidebar"; // we'll create
